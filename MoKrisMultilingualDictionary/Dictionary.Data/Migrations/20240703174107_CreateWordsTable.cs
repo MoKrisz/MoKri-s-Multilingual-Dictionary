a07f3c56f587f80a786c74f716d06f63b4835b07
MoKrisz <79792553+MoKrisz@users.noreply.github.com>
2024-07-03 19:41:30 +0200

    add Words migration

---
+++ MoKrisMultilingualDictionary/Dictionary.Data/Migrations/20240703174107_CreateWordsTable.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Dictionary.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateWordsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "dictionary");
+
+            migrationBuilder.CreateTable(
+                name: "Words",
+                schema: "dictionary",
+                columns: table => new
+                {
+                    WordId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Article = table.Column<string>(type: "text", nullable: true),
+                    Text = table.Column<string>(type: "text", nullable: false),
+                    Plural = table.Column<string>(type: "text", nullable: true),
+                    Type = table.Column<int>(type: "integer", nullable: false),
+                    Conjugation = table.Column<string>(type: "text", nullable: true),
+                    LanguageCode = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Words", x => x.WordId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Words",
+                schema: "dictionary");
+        }
+    }
+}
