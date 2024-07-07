e42cda99c904df10fd3ad18daa9f036fce2df259
MoKrisz <79792553+MoKrisz@users.noreply.github.com>
2024-07-07 19:47:02 +0200

    prepare word components for test

--- MoKrisMultilingualDictionary/Dictionary.Model/Dtos/WordDto.cs
+++ MoKrisMultilingualDictionary/Dictionary.Model/Dtos/WordDto.cs
@@ -1,20 +1,28 @@
-﻿using Dictionary.Models.Enums;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text.Json.Serialization;
 
 namespace Dictionary.Models.Dtos
 {
     public class WordDto
     {
+        [JsonPropertyName("wordId")]
         public int WordId { get; set; }
+
+        [JsonPropertyName("article")]
         public string? Article { get; set; }
+
+        [JsonPropertyName("text")]
         public string Text { get; set; } = string.Empty;
+
+        [JsonPropertyName("plural")]
         public string? Plural { get; set; }
+
+        [JsonPropertyName("type")]
         public int Type { get; set; }
+
+        [JsonPropertyName("conjugation")]
         public string? Conjugation { get; set; }
+
+        [JsonPropertyName("languageCode")]
         public int LanguageCode { get; set; }
     }
 }
