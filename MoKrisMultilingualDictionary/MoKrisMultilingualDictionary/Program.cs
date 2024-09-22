f6a3f883d55b953d47265eabf7b1baeeb63447c5
MoKrisz <79792553+MoKrisz@users.noreply.github.com>
2024-09-22 15:36:29 +0200

    setup cors for the frontend.

--- MoKrisMultilingualDictionary/MoKrisMultilingualDictionary/Program.cs
+++ MoKrisMultilingualDictionary/MoKrisMultilingualDictionary/Program.cs
@@ -18,6 +18,13 @@ builder.Services.AddAutoMapper(typeof(Program));
 
 builder.Services.AddMediatR(config => config.RegisterServicesFromAssembly(typeof(GetWordHandler).Assembly));
 
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(MyAllowSpecificOrigins, policy =>
+        policy.WithOrigins("http://localhost:5173"));
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -28,6 +35,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseAuthorization();
 
