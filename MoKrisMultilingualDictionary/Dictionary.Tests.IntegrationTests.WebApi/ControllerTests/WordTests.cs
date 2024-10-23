abf653228bc2683f5066faa62744afc4b598c486
MoKrisz <79792553+MoKrisz@users.noreply.github.com>
2024-10-23 12:58:35 +0200

    post word test

--- MoKrisMultilingualDictionary/Dictionary.Tests.IntegrationTests.WebApi/ControllerTests/WordTests.cs
+++ MoKrisMultilingualDictionary/Dictionary.Tests.IntegrationTests.WebApi/ControllerTests/WordTests.cs
@@ -51,5 +51,36 @@ namespace Dictionary.Tests.IntegrationTests.WebApi.ControllerTests
             result.Type.Should().Be((int)word.Type);
             result.LanguageCode.Should().Be((int)word.LanguageCode);
         }
+
+        [Fact]
+        public async Task PostWordTest()
+        {
+            var dbContext = await fixture.GetDatabase();
+
+            var wordDto = new WordDto
+            {
+                Article = "die",
+                Text = "Mutter",
+                Plural = "Mütter",
+                Type = (int)WordTypeEnum.Noun,
+                LanguageCode = (int)LanguageCodeEnum.DE
+            };
+
+            var url = $"/{WordController.PostWordRoute}";
+            var response = await client.PostAsJsonAsync(url, wordDto);
+            response.EnsureSuccessStatusCode();
+
+            var result = await response.Content.ReadAsStringAsync();
+            var resultId = Convert.ToInt32(result);
+
+            dbContext.Words.Count().Should().Be(1);
+            var assertWord = await dbContext.Words.FirstAsync();
+            assertWord.WordId.Should().Be(resultId);
+            assertWord.Article.Should().Be(wordDto.Article);
+            assertWord.Text.Should().Be(wordDto.Text);
+            assertWord.Plural.Should().Be(wordDto.Plural);
+            assertWord.Type.Should().Be(WordTypeEnum.Noun);
+            assertWord.LanguageCode.Should().Be(LanguageCodeEnum.DE);
+        }
     }
 }
