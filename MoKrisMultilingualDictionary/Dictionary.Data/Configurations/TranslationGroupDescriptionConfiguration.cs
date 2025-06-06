c387b7c4c2c257634b7b5e997254773861f18eff
MoKrisz <79792553+MoKrisz@users.noreply.github.com>
2025-06-06 20:30:27 +0200

    configs

---
+++ MoKrisMultilingualDictionary/Dictionary.Data/Configurations/TranslationGroupDescriptionConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Dictionary.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Dictionary.Data.Configurations
+{
+    public class TranslationGroupDescriptionConfiguration : IEntityTypeConfiguration<TranslationGroupDescription>
+    {
+        public void Configure(EntityTypeBuilder<TranslationGroupDescription> builder)
+        {
+            builder.Property(tgd => tgd.Description)
+                .HasMaxLength(100)
+                .IsRequired();
+
+            builder.HasIndex(tgd => tgd.Description);
+
+            builder.HasOne(tgd => tgd.TranslationGroup)
+                .WithOne(tg => tg.TranslationGroupDescription)
+                .HasForeignKey<TranslationGroupDescription>(tgd => tgd.TranslationGroupId)
+                .OnDelete(DeleteBehavior.NoAction);
+        }
+    }
+}
