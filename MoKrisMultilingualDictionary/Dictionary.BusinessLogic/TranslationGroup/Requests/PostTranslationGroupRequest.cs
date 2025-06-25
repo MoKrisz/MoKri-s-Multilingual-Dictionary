5527d0ef65d5ab37e6eefeb89c0ea124d6de949c
MoKrisz <79792553+MoKrisz@users.noreply.github.com>
2025-06-25 21:32:10 +0200

    implement translation group creation

---
+++ MoKrisMultilingualDictionary/Dictionary.BusinessLogic/TranslationGroup/Requests/PostTranslationGroupRequest.cs
@@ -0,0 +1,10 @@
+﻿using Dictionary.Models.Dtos;
+using MediatR;
+
+namespace Dictionary.BusinessLogic.TranslationGroup.Requests
+{
+    public class PostTranslationGroupRequest : IRequest<int>
+    {
+        public TranslationGroupDto TranslationGroup { get; set; } = default!;
+    }
+}
