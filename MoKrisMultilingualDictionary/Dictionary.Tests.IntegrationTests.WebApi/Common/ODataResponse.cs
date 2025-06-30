ee46bb33001924cad68ad974631dd2cdb7fccf8f
MoKrisz <79792553+MoKrisz@users.noreply.github.com>
2025-06-30 20:42:55 +0200

    feat: implement translation group odata endpoint and tests for it

---
+++ MoKrisMultilingualDictionary/Dictionary.Tests.IntegrationTests.WebApi/Common/ODataResponse.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Dictionary.Tests.IntegrationTests.WebApi.Common
+{
+    public class ODataResponse<T>
+    {
+        [JsonPropertyName("@odata.count")]
+        public int Count { get; set; }
+
+        [JsonPropertyName("value")]
+        public List<T> Values { get; set; } = default!;
+    }
+}
