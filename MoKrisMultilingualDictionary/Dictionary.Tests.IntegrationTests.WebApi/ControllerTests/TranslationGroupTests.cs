48cda8098160ab53d669bb5f080880ee43157d61
MoKrisz <79792553+MoKrisz@users.noreply.github.com>
2025-07-11 19:31:51 +0200

    feat: implemented tests for the word related translation groups endpoint.

--- MoKrisMultilingualDictionary/Dictionary.Tests.IntegrationTests.WebApi/ControllerTests/TranslationGroupTests.cs
+++ MoKrisMultilingualDictionary/Dictionary.Tests.IntegrationTests.WebApi/ControllerTests/TranslationGroupTests.cs
@@ -1,9 +1,11 @@
 ﻿using Dictionary.Domain.Builders;
+using Dictionary.Domain.Enums;
 using Dictionary.Models.Dtos;
 using FluentAssertions;
 using Microsoft.EntityFrameworkCore;
 using MoKrisMultilingualDictionary.Routes;
 using System.Net.Http.Json;
+using System.Text.Json;
 
 namespace Dictionary.Tests.IntegrationTests.WebApi.ControllerTests
 {
@@ -62,6 +64,79 @@ namespace Dictionary.Tests.IntegrationTests.WebApi.ControllerTests
             }
 
             (await db.Tags.CountAsync()).Should().Be(translationGroupDto.Tags.Count);
+        }
+
+        [Fact]
+        public async Task GetWordRelatedTranslationGroupsTest()
+        {
+            var db = await fixture.GetDatabase();
+
+            var linkedTranslationGroup = new TranslationGroupBuilder()
+                .SetDescription("Test1")
+                .Build();
+
+            var potentialTranslationGroup1 = new TranslationGroupBuilder()
+                .SetDescription("Test2")
+                .Build();
+
+            var potentialTranslationGroup2 = new TranslationGroupBuilder()
+                .SetDescription("Test3")
+                .Build();
+
+            var sourceWord = new WordBuilder()
+                .SetText("Test")
+                .SetPlural("Tests")
+                .SetType(WordTypeEnum.Noun)
+                .SetLanguageCode(LanguageCodeEnum.EN)
+                .Build();
+
+            var targetWord = new WordBuilder()
+                .SetArticle("Die")
+                .SetText("Test")
+                .SetPlural("Tests")
+                .SetType(WordTypeEnum.Noun)
+                .SetLanguageCode(LanguageCodeEnum.DE)
+                .Build();
+
+            var wordTranslationGroups = new []
+            {
+                new WordTranslationGroupBuilder()
+                    .SetWord(sourceWord)
+                    .SetTranslationGroup(linkedTranslationGroup)
+                    .Build(),
+                new WordTranslationGroupBuilder()
+                    .SetWord(targetWord)
+                    .SetTranslationGroup(linkedTranslationGroup)
+                    .Build(),
+                 new WordTranslationGroupBuilder()
+                    .SetWord(sourceWord)
+                    .SetTranslationGroup(potentialTranslationGroup1)
+                    .Build(),
+                new WordTranslationGroupBuilder()
+                    .SetWord(targetWord)
+                    .SetTranslationGroup(potentialTranslationGroup2)
+                    .Build(),
+            };
+
+
+            await db.Words.AddRangeAsync(sourceWord, targetWord);
+            await db.TranslationGroups.AddRangeAsync(linkedTranslationGroup, potentialTranslationGroup1, potentialTranslationGroup2);
+            await db.WordTranslationGroups.AddRangeAsync(wordTranslationGroups);
+            await db.SaveChangesAsync();
+
+            var url = $"/{TranslationGroupRoutes.ControllerBaseRoute}/{TranslationGroupRoutes.WordRelatedTranslationGroupsRoute}?sourceWordId={sourceWord.WordId}&targetWordId={targetWord.WordId}";
+            var response = await client.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+
+            var jsonResult = await response.Content.ReadAsStringAsync();
+            var result = JsonSerializer.Deserialize<WordRelatedTranslationGroupsDto>(jsonResult);
+
+            result.Should().NotBeNull();
+            result!.LinkedTranslationGroups.Count.Should().Be(1);
+            result.LinkedTranslationGroups[0].TranslationGroupId.Should().Be(linkedTranslationGroup.TranslationGroupId);
+            result.PotentialTranslationGroups.Count.Should().Be(2);
+            result.PotentialTranslationGroups.Any(ptg => ptg.TranslationGroupId == potentialTranslationGroup1.TranslationGroupId).Should().BeTrue();
+            result.PotentialTranslationGroups.Any(ptg => ptg.TranslationGroupId == potentialTranslationGroup2.TranslationGroupId).Should().BeTrue();
         }
     }
 }
